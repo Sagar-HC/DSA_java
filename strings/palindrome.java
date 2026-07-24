bf13ff23b1531b61d2d0a7ec4b512a2261701118
Sagar Hc <ssag07906@example.com>
2026-07-24 20:00:46 +0530

    shortest distance

--- strings/palindrome.java
+++ strings/palindrome.java
@@ -9,8 +9,40 @@ public class palindrome {
         }return true;
     }
 
+    public static float shortestPath(String str){
+        int n = str.length();
+        int x= 0;
+        int y = 0; 
+        for (int i = 0; i<n;i++){
+            int c = str.charAt(i);
+            //north
+            if(c == 'n'){
+                y++;
+            }
+            //south
+            else if(c == 's'){
+                y--;
+            }
+            //east
+            else if(c == 'e'){
+                x++;
+            }
+            //west
+            else if(c == 'w'){
+                x--;
+            }
+            
+        }int X2 = x*x;
+        int Y2 = y*y;
+        return (float)Math.sqrt(Y2+X2);
+            
+    }
+
     public static void main(String[] args) {
-        String str1 = "racecar";
-        System.out.println(isPalindrome(str1));
+        // String str1 = "racecar";
+        // System.out.println(isPalindrome(str1));
+        String str10 = "nnwwsseeeeesswssee";
+        System.out.print(shortestPath(str10));
     }
 }
+
