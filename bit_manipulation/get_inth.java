7358440b8a0855fcf75eb2977c9b987f71031376
Sagar Hc <ssag07906@gmail.com>
2026-08-07 23:09:34 +0530

    odd_even code in bitwise

---
+++ bit_manipulation/get_inth.java
@@ -0,0 +1,14 @@
+package bit_manipulation;
+
+public class get_inth {
+    public static  int ith(int n , int i){
+        int bitmask = 1<<i;
+        if((bitmask & n) == 0) {
+            return 0;
+        }else{return 1;}
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ith(10,2));
+    }
+}
