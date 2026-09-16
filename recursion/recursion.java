f79d08afc50aadc149a165ab0806630acfd275ae
Sagar Hc <ssag07906@gmail.com>
2026-09-16 21:54:40 +0530

    git push origin main

--- recursion/recursion.java
+++ recursion/recursion.java
@@ -1,5 +1,35 @@
 package recursion;
 
 public class recursion {
-    
+    public static void recurse(int n){
+        if( n == 1){
+            System.out.print("1");
+            return ;
+        }
+        System.out.println(n);
+        recurse(n-1);
+    }
+     public static void recurseInverse(int n){
+        
+          if( n == 1){
+            System.out.println("1");
+            return ;
+        }
+        recurseInverse(n-1);
+        System.out.println(n);
+       
+
+      
+
+    }
+
+
+
+    public static void main(String args[]){
+        recurse(10);
+        recurseInverse(10);
+    }
+
 }
+
+
